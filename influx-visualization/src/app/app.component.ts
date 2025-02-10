0b207db7f014536ecbca4e738cdd8afdb3afcadf
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2025-02-09 20:18:46 -0800

    got server to actually  work

--- influx-visualization/src/app/app.component.ts
+++ influx-visualization/src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule],
+  template: `
+    <h1>Welcome to {{ title }}!</h1>
+    <div *ngIf="message">Backend Message: {{ message }}</div>
+    <div *ngIf="error">Error: {{ error }}</div>
+  `,
 })
-export class AppComponent {
-  title = 'influx-visualization';
+export class AppComponent implements OnInit {
+  title = 'My Angular App';
+  message: string = '';
+  error: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any>('/api/test').subscribe({
+      next: (response) => {
+        this.message = response.message;
+        console.log('Response:', response);
+      },
+      error: (error) => {
+        this.error = 'Could not connect to backend: ' + error.message;
+        console.error('Backend connection error:', error);
+      },
+    });
+  }
 }
