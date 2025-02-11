f56c4767e596d265be167e3625c4d70fffc7a9d7
Ari Ferreros <tinytabby66@gmail.com>
2025-02-11 10:44:49 -0800

    widget component

--- ari/lab-monitor/src/app/components/widget-view/widget-view.component.ts
+++ ari/lab-monitor/src/app/components/widget-view/widget-view.component.ts
@@ -8,6 +8,6 @@ import { WidgetComponent } from '../widget/widget.component';
   styleUrl: './widget-view.component.scss'
 })
 export class WidgetViewComponent {
-  //dynamically display array of Widget Components
+  //dynamically display array of Widget Components, each should have unique ID
 }
 
